9f2137a9a264aa51bb9858e337108d7c2af11f68
felipefausto42 <fellipe.fausto10@gmail.com>
2023-11-28 14:33:24 -0300

    Erros completedItem corrigidos

--- Scripts/Miscellaneous/CompletedItem.gd
+++ Scripts/Miscellaneous/CompletedItem.gd
@@ -10,14 +10,22 @@ extends Control
 @export var idExperimento = 7
 
 @onready var nomeExperimento = $NomeExperimento
-	
+
+var hasBeenCalled = false	
 	
 func _process(delta):
+	if GameManager.correct_experiments.has(idExperimento) and !hasBeenCalled:
+		reloadIcons()
+		hasBeenCalled = true
+		
+func reloadIcons():
 	if GameManager.correct_experiments.has(idExperimento):
 		$Meteriais/Sprite2D.texture = load(material1)
 		$Meteriais/Sprite2D2.texture = load(material2)
 		$Meteriais/Sprite2D3.texture = load(material3)
-		$Meteriais/Sprite2D4.texture = load(material4)
-		$Meteriais/Sprite2D5.texture = load(material5)
+		if experimento == "Bexiga Cheia" or experimento == "Cobra de Fogo":
+			$Meteriais/Sprite2D4.texture = load(material4)
+		if experimento == "Cobra de Fogo":
+			$Meteriais/Sprite2D5.texture = load(material5)
 		$Moldura/IconeExperimento.texture = load(iconeExperimento)
 		$NomeExperimento.text = experimento
